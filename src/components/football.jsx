30fcc87b56b62fa938cd09cd59c6acc4f52cf996
React <react.tehran@gmail.com>
2021-05-17 20:10:01 +0430

    api call done

--- src/components/football.jsx
+++ src/components/football.jsx
@@ -1,6 +1,20 @@
+import React, { useState, useEffect } from 'react';
 import './football.css';
+import axios from 'axios'
 
 function Football() {
+  const [count, setCount] = useState([]);
+  useEffect(() => {
+    let data = {
+      headers: {
+          "X-Auth-Token": "ea7cb0230e764362aef72269a19ed9d5",
+          "content-type": "application/json"
+      }
+  };
+    axios.get('https://api.football-data.org/v2/competitions/2021/matches', data)
+        .then(response => setCount(...count, response.data.matches));
+
+}, []);
     return (
       <div className="Main">
        <div className="title">
