47a1cd7b67b9a4b6f99ed5f2db3d7ad6ef3c788d
React <react.tehran@gmail.com>
2021-05-23 20:39:46 +0430

    fix dropdown styles

--- src/components/football.jsx
+++ src/components/football.jsx
@@ -3,7 +3,10 @@ import './football.css';
 import axios from 'axios'
 
 function Football() {
-  const [count, setCount] = useState([]);
+  const[selectedValue, setSelectedValue] = useState("هفته1")
+  const [showText, setShowText] = useState(false);
+  const [week, setWeek] = useState([]);
+  const [matche, setMatch] = useState([]);
   useEffect(() => {
     let data = {
       headers: {
@@ -12,21 +15,43 @@ function Football() {
       }
   };
     axios.get('https://api.football-data.org/v2/competitions/2021/matches', data)
-        .then(response => setCount(...count, response.data.matches));
+        .then(response => setMatch(...matche, response.data.matches));
 
 }, []);
+  const chunkSize = 10;
+  const arr = matche;
+  const groups = arr.map((e, i) => {
+    return i % chunkSize === 0 ? arr.slice(i, i + chunkSize) : null; 
+  }).filter(e => { return e; });
+  groups.forEach((item, i) => {
+    item.id = i + 1;
+  });
+  const openModal = () => setShowText(true);
+  const closeModal = () => setShowText(false);
+  console.log(showText)
+
+    console.log(groups);
     return (
       <div className="Main">
        <div className="title">
            برنامه بازی ها
        </div>
        <div className="flexStyle">
-        <select className="select" id="cars" name="carlist" form="carform">
-          <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
-          <option value="opel">Opel</option>
-          <option value="audi">Audi</option>
-        </select>
+        <div className="kkk">
+          <button className="buttonWeeks" onClick={openModal}>هفته ها</button>
+          {showText ? 
+            <div className="modal">
+              <div className="flexStyle1">
+                <button className="closeButton" onClick={closeModal}>بستن</button>
+              </div>
+              <div className="flexStyle2">
+                {groups.map(item => {
+                  <li className="options">{item.value}</li>
+                })}
+              </div>
+            </div> 
+          : null}
+        </div>
        </div>
       </div>
     );
